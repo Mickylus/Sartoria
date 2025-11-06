9e4588bf5601bd9fae0245589f375a2378414828
Michele Valiati <valiatimichele07@gmail.com>
2025-11-06 11:55:00 +0100

    06/11/2025 - Iniziato progetto sartoria

---
+++ Sartoria.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+// Gruppo: Michele Valiati & Filippo Mancini
+// Progetto Sartoria
+
+/*
+Analisi:
+	Struct:
+
+	Funzioni:
+
+	Variabili:
+
+	Operazioni:
+
+*/
+
+int main(){
+	return 0;
+}
