aa9598740610f836ba7fecd19ad3cae7bb2d92d1
Michele Valiati <valiatimichele07@gmail.com>
2025-11-07 19:29:11 +0100

    Iniziata struttura dati

--- Codice/Sartoria.c
+++ Codice/Sartoria.c
@@ -5,6 +5,16 @@
 // Gruppo: Michele Valiati & Filippo Mancini
 // Progetto Sartoria
 
+struct rotolo{
+	char tipo_tessuto[20];
+	char colore[20];
+	char fantasia[20];
+	float lunghezza;
+	float larghezza;
+	char codice_fornitura[20];
+};
+
+
 
 int main(){
 	return 0;
