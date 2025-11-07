3c25cd984d6d916639f5b33a91972a3050c5e7de
Michele Valiati <valiatimichele07@gmail.com>
2025-11-07 20:31:34 +0100

    Create prime structs

--- Codice/Sartoria.c
+++ Codice/Sartoria.c
@@ -5,17 +5,39 @@
 // Gruppo: Michele Valiati & Filippo Mancini
 // Progetto Sartoria
 
+#define MAXSTRING 30
+#define MAXTESSUTI 100
+
+
 struct rotolo{
-	char tipo_tessuto[20];
-	char colore[20];
-	char fantasia[20];
+	char tipo_tessuto[MAXSTRING];
+	char colore[MAXSTRING];
+	char fantasia[MAXSTRING];
 	float lunghezza;
 	float larghezza;
-	char codice_fornitura[20];
+	char codice_fornitura[MAXSTRING];
+	float costo;
+	float usura;
 };
+
+struct data{
+	int g;
+	int m;
+	int a;
+};
+
+struct scheda{
+	char codice_rotolo[MAXSTRING];
+	char fornitore[MAXSTRING];
+	struct rotolo rot;
+	struct data data_acquisto;
+	float quantita_disponibile;
+	float utilizzo_previsto;
+} inventario[MAXTESSUTI];
 
 
 
 int main(){
+	
 	return 0;
 }
